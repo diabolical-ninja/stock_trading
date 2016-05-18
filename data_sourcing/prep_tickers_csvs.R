6c5dcc58696ccc7ec190cc9e3e4f10c8308772a8
Yassin Eltahir <yeltahir@gmail.com>
2016-05-18 23:30:56 +0800

    Add script to clean and prep raw ticker csv's

---
+++ data_sourcing/prep_tickers_csvs.R
@@ -0,0 +1,40 @@
+##****************************************************************************************
+# Title: Prep Tickers
+#
+# Desc: List of all Yahoo stock tickers extract from spreadsheet to csv's. 
+#       Each csv is saved as pipe delimited csv's. This also addresses the weird 
+#       line escaping caused by creating csv's from excel (using OSX).
+#       Spreadsheet from http://investexcel.net/all-yahoo-finance-stock-tickers/
+#        ** The works these guys provide is super helpful!! **
+#
+# Author:  2016-05-18  Yassin Eltahir
+#
+# Steps:
+#
+#
+##****************************************************************************************
+
+
+# 0. Setup Environment  ####
+
+# Load Required Packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(data.table)
+
+
+
+# 1. Source Files, Clean, Output  ####
+
+# Get all files like "ticker_*"
+files <- list.files('ref/',pattern = 'tickers')
+
+# Read in, and save as pipe delimited csv
+for(i in files){
+  
+  # Read
+  ticker <- fread(sprintf('ref/%s',i))
+  
+  # Output
+  write.table(ticker, sprintf('ref/%s',i), quote = F, sep = '|', row.names = F)
+  
+}
