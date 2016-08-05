56abc5c085184ceedf79e090147507ed5c5e7a62
yassin-eltahir <yassin-eltahir@users.noreply.github.com>
2016-08-05 16:05:55 +1000

    Source stock headlines

---
+++ data_sourcing/headline_scraper.py
@@ -0,0 +1,30 @@
+# Import Required Packages
+import feedparser
+import pandas as pd
+import tqdm
+
+
+# Tickers
+ticker_file = 'companies-2016-2-1470371480687.csv'
+codes = pd.read_csv(ticker_file, delimiter=',')
+
+# Variable to populate with articles
+headline = []
+
+# Loop through each ticker
+for ticker in tqdm.tqdm(codes.Code):
+    
+    # Build Headlines URL string
+    url = 'https://feeds.finance.yahoo.com/rss/2.0/headline?s=' + ticker + '&region=US&lang=en-US'
+    
+    # Source headlines
+    info = feedparser.parse(url)
+    
+    # Extract title, link & date
+    for entry in info.entries:
+        
+        if entry.title != 'Yahoo! Finance: RSS feed not found':
+            headline.append([ticker,entry.title, entry.link, entry.published])
+
+articles = pd.DataFrame(headline)
+
