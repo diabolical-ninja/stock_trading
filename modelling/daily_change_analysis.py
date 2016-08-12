6e23e50fc80c001008c4cf7430b52621fa4073d0
Yassin Eltahir <yassin.eltahir@iselect.com.au>
2016-08-12 16:08:27 +1000

    Add working code to plot % change

--- modelling/daily_change_analysis.py
+++ modelling/daily_change_analysis.py
@@ -5,8 +5,6 @@
 #           - % & $ Diff
 #           - % & $ Dollar Volume
 #
-#       https://www.quantopian.com/algorithms/57a7346c4b93e1d1a3000755#algorithm
-#
 # Inputs: eod_initial.csv
 #
 #********************************************************************************
@@ -16,63 +14,69 @@
 
 import pandas as pd
 import numpy as np
-#import plotly.plotly as py
-#import plotly.graph_objs as go
 from datetime import datetime
+import plotly.graph_objs as go
+from plotly import tools
+from plotly.offline import plot
 
 
-from plotly.offline import download_plotlyjs, init_notebook_mode, iplot
-from plotly.graph_objs import *
 
 
 # 2. Source Data & Transform
 
-eod = pd.read_csv('/users/yassineltahir/Repos/stock_trading/Data/eod_initial.csv',sep='|')
+#eod = pd.read_csv('/users/yassineltahir/Repos/stock_trading/Data/eod_initial.csv',sep='|')
+eod = pd.read_csv('C:/Users/yassin.eltahir/Documents/Repositories/stock_trading/Data/eod_initial.csv',sep='|')
 eod['Date'] = pd.to_datetime(eod.Date)
 
 
 # Calculate Daily $ Change
 eod['dol_change'] = eod.Close - eod.Open
 
-# Calculate Daily $ Change
+# Calculate Daily % Change
 eod['pct_change'] = eod.dol_change/eod.Open
 
 
-
-# Calculate Dollar Volume using average day price (open/close)
-eod['open_close_avg'] = (eod.Open + eod.Close)/float(2)
-eod['dollar_volume'] = eod.Volume * eod.open_close_avg
-
-
-## Calculate Dollar Volume $ Change
-#eod['dv_dol_change'] = 
-
-
-
-# Calculate Rolling Average over 2wk window
+# Calculate Rolling Average over window
 #eod = eod.sort_values
-eod['rolling_pct_change'] = eod.groupby('ticker')['pct_change'].apply(pd.rolling_mean, 10, min_periods=5)
+eod['rolling_pct_change'] = eod.groupby('ticker')['pct_change'].apply(pd.rolling_mean, 5, min_periods=3)
+eod['rolling_pct_change'] = eod['rolling_pct_change'] * float(100)
 
 
 # Calculate Average pct change per ticker & find highest values
-ave_pct_change = eod[eod.Date > '2016-07-01'].groupby('ticker')['pct_change'].apply(np.mean).reset_index('ticker')
+ave_pct_change = eod[eod.Date > '2016-08-01'].groupby('ticker')['pct_change'].apply(np.mean).reset_index('ticker')
 top_5 = ave_pct_change.sort_index(by='pct_change', ascending=False).head(5)
-
-# Required Plotly packages
-import plotly.graph_objs as go
-from plotly import tools
-from plotly.offline import plot
+bottom_5 = ave_pct_change.sort_index(by='pct_change', ascending=True).head(5)
 
 
-# Plot top 5 stocks
-data = go.Scatter(
-          x=eod[(eod.Date > '2016-07-01') & (eod.ticker=='WHC')].Open,
-          y=eod[(eod.Date > '2016-07-01') & (eod.ticker=='WHC')]['pct_change']
-         )
 
-#go.Figure(data=data)
-#py.iplot(data)
-plot(fig, filename='%s/%s_heat_map_%s'%(dir_name,script_name, ds))
+# 3. Plot Top 5 Stocks
+
+# Plot 5 based on average % change over the last 1.5 months
+tickers_of_interest = np.array(top_5.ticker)
+#tickers_of_interest = np.array(bottom_5.ticker)
 
 
-py.image.save_as(data, filename='test_plot')
+# Plot Data
+data = [go.Scatter(x = eod[(eod.Date > '2016-07-01') & (eod.ticker==ticker)].Date,
+                   y=eod[(eod.Date > '2016-07-01') & (eod.ticker==ticker)]['rolling_pct_change'],
+                   name=ticker
+               ) for ticker in tickers_of_interest]
+
+# Plot Nicities
+layout = go.Layout(
+    title= '5 day Rolling Average of % Change (Close - Open)',
+    yaxis=dict(title='% Change'),
+    xaxis=dict(title='Date')
+    )
+    
+# Combine Plot & Formatting
+fig = go.Figure(data=data, layout=layout)
+
+# Generate Plot
+plot(fig, filename='test')
+
+
+
+# 4. Plot Stoct Avg Change with Actual Change
+
+
