f3bba18c5fe8da2aeabcab9aec1ef58f2e97028e
Yassin Eltahir <yassin.eltahir@iselect.com.au>
2016-08-19 16:02:20 +1000

    Start making mean reversion function

--- modelling/mean_reversion_v1.py
+++ modelling/mean_reversion_v1.py
@@ -19,24 +19,25 @@ import math
 
 # 2. Source Data & Transform
 
-eod = pd.read_csv('/users/yassineltahir/Repos/stock_trading/Data/eod_initial.csv',sep='|')
+#eod = pd.read_csv('/users/yassineltahir/Repos/stock_trading/Data/eod_initial.csv',sep='|')
+eod = pd.read_csv('C:/Users/yassin.eltahir/Documents/Repositories/stock_trading/Data/eod_initial.csv',sep='|')
 eod['Date'] = pd.to_datetime(eod.Date)
 
 
-# Look at only last 6months of data
-min_date = datetime.datetime.today() - datetime.timedelta(days=180)
-eod = eod[eod.Date > min_date]
+## Look at only last 6months of data
+#min_date = datetime.datetime.today() - datetime.timedelta(days=180)
+#eod = eod[eod.Date > min_date]
 
 
 
 
-
+# Select Ticker
 ticker = np.unique(eod.ticker)[20]
 test = eod[eod.ticker==ticker]
 
-trade_amount=1000
+trade_amount=10000
 shares_owned = 0
-brokerage = 20
+brokerage = .20
 expenses = 0
 revenue = 0
 
@@ -166,7 +167,7 @@ plot(fig, filename='test')
 # 3. Mean By/Sell Function
 
 
-def mean_reversion(data,trade_amount,mean_window,brokerage):
+def mean_reversion(data,dol_buy,mean_window,brokerage, mean_type, date_name, buy_sell_buffer):
     
     """
     Simple Mean Reversion Stratergy
@@ -174,10 +175,107 @@ def mean_reversion(data,trade_amount,mean_window,brokerage):
     - If price goes above N-day rolling average then sell
     - If price goes below N-day rolling average then buy
     - Trades happen once per day
+    
+    Inputs:
+        - data: Dataframe containing ticker data
+        - dol_buy: Amount ($'s) of stock to buy each time
+        - mean_window: Window size to calculate rolling averages
+        - brokerage: Cost of brokerage per transaction
+        - mean_type: Rolling function to use, eg EWMA
+        - date_name: Name of timestamp column to use
+        - buy_sell_buffer: How far above/below mean to buy/sell as a %
+        
+    """
+
+    # Initialise Variables
+    shares_owned = 0
+    expenses = 0
+    revenue = 0
+    trade_info = []
+
+    
+    # 1. Sort By Date Ascending
+    date_name = 'Date'
+    data = data.sort_values(by=date_name)
+    
+    # 2. Calculate rolling average of size "mean_window"
+    if mean_type == "MA":  # Moving Average
+        roll = pd.DataFrame({'Date':data['Date'],
+                         'rolling_open':data['Open'].rolling(window = mean_window, min_periods = mean_window).mean()
+                         })
+    elif mean_type == "EWMA": #Exponentially Weighted Moving Average
+        roll = pd.DataFrame({'Date':data['Date'],
+                         'rolling_open':pd.ewma(data['Open'],span = mean_window  ,min_periods = mean_window)
+                         })
+    
+    # Align the rolling mean with tomorrows date
+    # Want to check current price against yesterdays average (don't know todays)
+    roll['Date'] = roll['Date'] + datetime.timedelta(days=1)
+    
+    # Join to Ticker data
+    data['rolling_open'] = roll.rolling_open
 
 
+    # 3. Buy/Sell Action
 
+    # For each day, compare min(high, close) to rolling mean
+    for day in data[mean_window:data.shape[0]].iterrows():
 
+        # Check if open is above/below mean
+        if day[1]['Open'] > day[1]['rolling_open'] * (1+buy_sell_buffer):
+            action = 'sell'
+        elif day[1]['Open'] < day[1]['rolling_open']*(1-buy_sell_buffer):
+            action = 'buy'
+        else:
+            action = 'Nothing'
+               
+        
+        if action == 'sell':
+            # Identify min(High, Close)
+            # Chosen so to pick "Worst" return of the 2
+            price = min(day[1]['High'],day[1]['Close'])
+            
+            # Sell Shares
+            if shares_owned != 0:
+                income = price * shares_owned - brokerage
+            else:
+                income = 0
+            
+            revenue = revenue + income
+            
+            # Reset number of shares owned
+            shares_owned = 0
+            
+            
+        if action == 'buy':
+            
+            # Calculate number of stocks to buy given max dol_buy
+            shares_bought = math.floor(dol_buy/day[1]['Open'])
+            
+            # Cumulate number of numebrs
+            shares_owned = shares_owned + shares_bought
+            
+            # Cumulate transaction cost
+            expenses = expenses + shares_bought*day[1]['Open'] + brokerage
+        
+        
+        # 4. Capture Daily Action Details
+        
+        # Collect Days Trading Information
+        out = [day[1],  # Initial Data
+               action,  # What Happened
+               income,  # If a sale, how much money did it bring in
+               shares_bought*day[1]['Open'] + brokerage if action=='buy' else 0, # If a purchase, what was the cost
+               shares_bought if action=='buy' else 0,
+               shares_owned,
+               revenue - expenses]
+       
+       trade_info.append(out)
+         
+    # Format for fn output    
+    trade_info= pd.DataFrame(trade_info)
+    cashFlow.columns = ['Date','action','cumulative_profit', 'Sell','Buy','sell_buy','Open','Close','High','Rolling_Open','shares_owned']         
+         
 
 # FN COMMENTS
 # Might be worth having an input be a list of column name mappings
